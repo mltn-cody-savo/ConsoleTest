ada5fd4b0232cc099cdfd32e291a2cc18af71693
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-08 15:49:55 -0500

    initial commit

---
+++ AWS_HelloWord_Pipeline_Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS_HelloWord_Pipeline_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Upate");
+        }
+    }
+}
