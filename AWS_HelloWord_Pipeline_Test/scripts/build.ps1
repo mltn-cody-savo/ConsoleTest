c3bd547ab2c554e13e4f78d16c2b0130d2b1f571
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-09 17:21:14 -0500

    update scripts

--- AWS_HelloWord_Pipeline_Test/scripts/build.ps1
+++ AWS_HelloWord_Pipeline_Test/scripts/build.ps1
@@ -8,15 +8,27 @@
 task default -depends BuildAndUnitTest
 
 task ValidateProperties {
-
+    Assert ($solutionFile -ne $null) "Solution cannot be null."
 }
 
 task Clean -depends ValidateProperties {
 
+
+    exec {
+      . nuget restore $workingDirectory\$solutionFile
+      . msbuild $workingDirectory\$solutionFile /t:Clean
+    }
 }
 
 task Build -depends ValidateProperties, Clean {
-    Exec { msbuild $solutionFile }
+  Write-Host Building: $workingDirectory\$solutionFile
+  Write-Host OutputPath = $workingDirectory\$buildPath
+
+    exec {
+        msbuild $workingDirectory\$solutionFile /t:Build /v:normal /p:OutputPath=$workingDirectory\$buildPath
+        Copy-Item $workingDirectory\$scriptsPath\scripts\ $workingDirectory\$buildPath\scripts -Recurse -PassThru | Write-Host;
+
+    }
 }
 
 task UnitTest {
