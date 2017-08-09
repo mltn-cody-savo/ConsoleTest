b7d420d94c0361435a6cb01180d2a5195e8b5516
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-09 17:01:23 -0500

    add build scripts

---
+++ AWS_HelloWord_Pipeline_Test/scripts/build.ps1
@@ -0,0 +1,26 @@
+﻿properties {
+    $solutionFile = $null;
+    $buildPath = $null;
+    $scriptsPath = $null;
+    $workingDirectory = $null;
+}
+
+task default -depends BuildAndUnitTest
+
+task ValidateProperties {
+
+}
+
+task Clean -depends ValidateProperties {
+
+}
+
+task Build -depends ValidateProperties, Clean {
+    Exec { msbuild $solutionFile }
+}
+
+task UnitTest {
+
+}
+
+task BuildAndUnitTest -depends Build, UnitTest
---
+++ AWS_HelloWord_Pipeline_Test/scripts/psake_build.ps1
@@ -0,0 +1,8 @@
+﻿param (
+    $scr_path,
+    $sln_path,
+    $buildPath
+)
+
+Import-Module psake.psm1;
+Invoke-psake build.ps1 -task @{scriptsPath=$scr_path;solutionFile=$sln_path;buildPath=$buildPath;workingDirectory=$PWD}
