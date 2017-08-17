8dfdcf5c32ba2b7644dc06493972fd10d70e09a2
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-17 17:16:57 -0500

    fix build script path

--- AWS_HelloWord_Pipeline_Test/scripts/psake_build.ps1
+++ AWS_HelloWord_Pipeline_Test/scripts/psake_build.ps1
@@ -4,5 +4,5 @@
     $buildPath
 )
 
-Import-Module .\psake.psm1;
+Import-Module .\AWS_HelloWord_Pipeline_Test\scripts\psake.psm1;
 Invoke-psake build.ps1 -task @{scriptsPath=$scr_path;solutionFile=$sln_path;buildPath='package';workingDirectory=$PWD}
