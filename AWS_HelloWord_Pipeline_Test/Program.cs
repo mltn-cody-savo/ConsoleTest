64db146a8ef78025744c0dc41ef006b2f0b8ae79
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-17 15:41:04 -0500

    update for new build

--- AWS_HelloWord_Pipeline_Test/Program.cs
+++ AWS_HelloWord_Pipeline_Test/Program.cs
@@ -7,7 +7,6 @@ namespace AWS_HelloWord_Pipeline_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // add line
             Console.WriteLine("Test Upate");
         }
     }
