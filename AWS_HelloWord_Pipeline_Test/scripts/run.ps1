8860f1806cf730c52ceaa2bee43b01d91d8e5b29
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-18 10:54:53 -0500

    add test script

---
+++ AWS_HelloWord_Pipeline_Test/scripts/run.ps1
@@ -0,0 +1,9 @@
+﻿param(
+	[string] $arg1
+)
+Write-Host "Script:             " $MyInvocation.MyCommand.Path
+Write-Host "Pid:                " $PID
+Write-Host "Host.Version:       " $Host.Version
+Write-Host "Is 64-bit process:  " $([Environment]::Is64BitProcess)
+Write-Host "Execution policy:   " $(Get-ExecutionPolicy)
+Write-Host "Arg1:               " $arg1
