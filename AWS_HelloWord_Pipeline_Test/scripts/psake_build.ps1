026d7380e912411f636410c8e8c3083f0b27002d
Milton B. Cody Jr <cody.mltn.dev@outlook.com>
2017-08-21 10:55:38 -0500

    update script files

--- AWS_HelloWord_Pipeline_Test/scripts/psake_build.ps1
+++ AWS_HelloWord_Pipeline_Test/scripts/psake_build.ps1
@@ -4,5 +4,32 @@
     $buildPath
 )
 
-Import-Module .\AWS_HelloWord_Pipeline_Test\scripts\psake.psm1;
-Invoke-psake  .\AWS_HelloWord_Pipeline_Test\scripts\build.ps1 -task @{scriptsPath=$scr_path;solutionFile=$sln_path;buildPath='package';workingDirectory=$PWD}
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+
+function execute-externaltool
+(
+  [string] $context,
+  [scriptblock] $actionBlock
+)
+{
+  # This function exists to check the exit code for the external tool called within the script block, so we don't have to do this for each call
+  & $actionBlock
+  if ($LastExitCode -gt 0) { throw "$context : External tool call failed" }
+}
+
+try{
+	Write-Host "Script:             " $MyInvocation.MyCommand.Path
+	execute-externaltool "Import paske module, and execute build script" { 
+		Import-Module .\AWS_HelloWord_Pipeline_Test\scripts\psake.psm1;
+		Invoke-psake  .\AWS_HelloWord_Pipeline_Test\scripts\build.ps1 -properties @{scriptsPath=$scr_path;solutionFile=$sln_path;buildPath='package';workingDirectory=$PWD}
+	}
+}
+catch {
+	Write-Host "$pid : Error caught - $_"
+	if($? -and (Test-Path variable:LastExitCode) -and $($LASTEXITCODE -gt 0)) {exit $LASTEXITCODE}
+	#The $? and $LastExitCode caters to both script and external process calls
+	else { exit 1}
+}
+
+
--- AWS_HelloWord_Pipeline_Test/scripts/psake_buildCopy.ps1
+++ AWS_HelloWord_Pipeline_Test/scripts/psake_buildCopy.ps1
@@ -1,10 +1,11 @@
-﻿param(
-	[string] $arg1
+﻿param (
+    $scr_path,
+    $sln_path,
+    $buildPath
 )
 
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
-
 
 function execute-externaltool
 (
@@ -18,21 +19,14 @@ function execute-externaltool
 }
 
 try{
-	Write-Host "Script:             " $MyInvocation.MyCommand.Path
-	Write-Host "Pid:                " $PID
-	Write-Host "Host.Version:       " $Host.Version
-	Write-Host "Is 64-bit process:  " $([Environment]::Is64BitProcess)
-	Write-Host "Execution policy:   " $(Get-ExecutionPolicy)
-	Write-Host "Arg1:               " $arg1
-
-	#Query a service that does not exist, sc.exe wil return with a non 0 exit code
-	execute-externaltool "Query a non existent service, will return with exit code != 0" {c:\windows\system32\sc.exe query service_does_not_exist }
+	Import-Module .\psake.psm1;
+	Invoke-psake  .\build.ps1 -task @{scriptsPath=$scr_path;solutionFile=$sln_path;buildPath='package';workingDirectory=$PWD}
 }
 catch {
 	Write-Host "$pid : Error caught - $_"
 	if($? -and (Test-Path variable:LastExitCode) -and $($LASTEXITCODE -gt 0)) {exit $LASTEXITCODE}
 	#The $? and $LastExitCode caters to both script and external process calls
 	else { exit 1}
-
 }
+
 
